3e74f1b5616e43c2f814f7f0a09e97dd243d0a6e
Michael Saure <info@msaure.com>
2016-01-20 21:29:34 +0100

    updated and merged Eclipse settings added IDEA and Eclipse configuration to .gitignore

--- src/main/java/com/msaure/jispel/vm/Registers.java
+++ src/main/java/com/msaure/jispel/vm/Registers.java
@@ -11,5 +11,53 @@ public class Registers {
     private Handle[] val;
     private int stackBase;
     private int stackOffset;
+    
+	public Handle getAcc() {
+		return acc;
+	}
+	
+	public void setAcc(Handle acc) {
+		this.acc = acc;
+	}
+	
+	public Handle getPc() {
+		return pc;
+	}
+	
+	public void setPc(Handle pc) {
+		this.pc = pc;
+	}
+	
+	public Environment getCe() {
+		return ce;
+	}
+	
+	public void setCe(Environment ce) {
+		this.ce = ce;
+	}
+	
+	public Handle[] getVal() {
+		return val;
+	}
+	
+	public void setVal(Handle[] val) {
+		this.val = val;
+	}
+	
+	public int getStackBase() {
+		return stackBase;
+	}
+	
+	public void setStackBase(int stackBase) {
+		this.stackBase = stackBase;
+	}
+	
+	public int getStackOffset() {
+		return stackOffset;
+	}
+	
+	public void setStackOffset(int stackOffset) {
+		this.stackOffset = stackOffset;
+	}
 
 }
